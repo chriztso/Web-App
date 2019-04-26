792cbf643312a47e1aeecc6769e4bff6ae7c5f3d
Chris Tso <christophertso2017@u.northwestern.edu>
2019-04-26 16:09:43 -0500

    Created working server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+var express = require("express");
+var bodyParser = require('body-parser')
+var app = express(); 
 
+var port = 3000; 
+
+
+app.use(bodyParser.json());
+
+app.post('/test', (req, res) => {
+    var returnString = '';
+   for(var i = 2; i < req.body["string_to_cut"].length; i+=3){
+       returnString += req.body["string_to_cut"][i];
+   }
+   res.send(returnString);
+})
+
+app.listen(port, () => {console.log(`Listening at ${port}`)});
